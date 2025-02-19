5d3fab976b98420550da9100665c047c57addce1
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-02-20 00:20:22 +0530

    updated javascript-questions folder with more questions

--- Lectures/Javascript/Questions/main.js
+++ Lectures/Javascript/Questions/main.js
@@ -62,3 +62,89 @@
 
 
 // 9. Check if a number is between 50 and 100.
+// var num = 74;
+// if (num >= 50 && num <= 100){
+//     console.log("Number is between 50 and 100");
+// }
+
+
+// 10.  Check if a person can enter a club (age >= 21 and has a membership card).
+// var person_age = 23;
+// var person_membership = "YES";
+
+// if (person_age >= 21 && person_membership){
+//     console.log("Persone can enter the Club");
+// }
+
+
+// 11.  Check if a number is divisible by 3 or 7.
+// var num = 63;
+// if (num % 3 == 0 && num % 7 == 0){
+//     console.log("Number is divisible by 3 and 7.");
+// }
+
+
+// 12.  Check if a person is eligible for a bank loan (age > 25 and salary > 30,000).
+// var age = 27;
+// var salary = 38000;
+
+// if (age > 25 && salary > 30000) {
+//     console.log("Person eligible for Loan");
+// }
+
+
+// 13.  Check if a user has admin access (role === "admin").
+// var role = "admin"
+// if (role == "admin"){
+//     console.log("Admin Access.")
+// }
+
+
+// 14.  Check if a product is available in stock (stock > 0).
+// var stock = 41;
+// if (stock > 0){
+//     console.log("Product Available.")
+// }
+
+
+// 15.  Check if a given year is a century year (ends with 00). 
+// var year = 2000;
+// if (year % 100 === 0){
+//     console.log("The year is a century year.")
+// }
+
+
+// 16.  Check if a number is zero.
+// var number = 0;
+// if (number == 0){
+//     console.log("The number is zero.")
+// }
+
+
+// 17.  Check if a person is a teenager (age between 13 and 19).
+// var age = 17;
+// if (age >= 13 && age <= 19){
+//     console.log("The person is a teenager.");
+// }
+
+
+// 18.  Check if a number is less than 100 and even.
+// var number = 56;
+// if (number <= 100 && number % 2 == 0){
+//     console.log("The number is less than 100 and even.");
+// }
+
+
+// 19.  Check if a string is empty.
+// var empty = "";
+// if (empty == ""){
+//     console.log("Empty String.");
+// }
+
+
+// 20.  Check if a number is a perfect square.
+var num = 25;
+var sqrt = Math.sqrt(num);
+if (){
+    console.log("The number is a perfect square");
+}
