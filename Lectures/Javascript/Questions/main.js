1e6f11f7914c6ee8dd7a5d82ffbe1bfb18d415cf
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-02-19 20:17:41 +0530

    created a questions folder in javascript folder to solve the questions in pdf

---
+++ Lectures/Javascript/Questions/main.js
@@ -0,0 +1,64 @@
+// 1. Check if a number is positive.
+// var b = 10;
+// if (b < 0){
+//     console.log("The Number "+ b +" is Negative")
+// }else{
+//     console.log("The Number "+ b +" is Positive")
+// }
+
+
+// 2. Check if a number is greater than 100. 
+// var num = 101;
+// if (num > 100){
+//     console.log("Number "+ num +" is greater than 100");
+// }else{
+//     console.log("Number is smaller than 100");
+// }
+
+
+// 3. Check if a person is eligible to vote (age >= 18).
+// var age = 20;
+
+// if (age >= 18){
+//     console.log("You can Vote !")
+// }else{
+//     console.log("Noo")
+// }
+
+
+// 4. Check if a number is a multiple of 5. 
+// var num = 45;
+// if (num % 5 == 0) {
+//     console.log("The number is a multiple of 5");
+// }
+
+
+// 5. Check if a given character is uppercase. 
+// var letter = 'R';
+// if (letter >= 'A' && letter <= 'Z'){
+//     console.log("The letter is Uppercase");
+// }
+
+
+// 6. Check if a number is a two-digit number.
+// var num = -47;
+// if (num >= 10 && num < 100 || num <= -10 && num > -99){
+//     console.log("The number is a two digit number");
+// }
+
+
+// 7. Check if a string contains the letter 'a'.
+// var str = "abhishikth";
+// if (str.includes('a')){
+//     console.log("The string contains the letter 'a'.");
+// }
+
+
+// 8. Check if a person is a senior citizen (age >= 60).
+// var age = 65;
+// if (age >= 60){
+//     console.log("The person is a Senior Citizen.");
+// }
+
+
+// 9. Check if a number is between 50 and 100.
