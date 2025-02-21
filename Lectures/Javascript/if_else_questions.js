37f68cd37ed3a306ce4aba6fed5f694501c6e158
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-02-21 19:34:45 +0530

    if_else_questions started

---
+++ Lectures/Javascript/if_else_questions.js
@@ -0,0 +1,7 @@
+// 1. Check if a number is even or odd
+// var number = 27;
+// if (number % 2 == 0){
+//     console.log("The number is Even.");
+// }else{
+//     console.log("The number is Odd.");
+// }
