8b066ab51f44431f702331e565c556170db1e485
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-03-05 19:50:26 +0530

    learned about objects and events in Javascript and added questions on for loop & patterns

---
+++ Lectures/Javascript/09/property.js
@@ -0,0 +1,9 @@
+// var car = "Abc";
+// console.log("Car : " + car);
+
+
+
+const car = {type:"ABC", model:"500", color:"white"};
+console.log("The car type is : " + car.type);
+console.log("The car model is : " + car.model);
+console.log("The car color is : " + car.color);
