f01cd7078baec6c499263a40539cbde82d1f9e31
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-03-05 23:32:45 +0530

    solved 6 questions of for loop in javascript

---
+++ Lectures/Javascript/Questions/for_loop/main.js
@@ -0,0 +1,41 @@
+// 1. Write a program to print numbers from 1 to 10 using a for loop.
+// for (var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+// 2. Print the even numbers between 1 and 20 using a for loop. 
+// for(var i = 1; i <= 20; i++){
+//     if (i%2 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+// 3. Print the multiplication table of 5 using a for loop.
+// for (var i = 1; i <= 10; i++){
+//     console.log(5*i);
+// }
+
+
+// 4. Find the sum of the first 10 natural numbers using a for loop.
+// var sum = 0;
+// for (var i = 1; i <= 10; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+
+// 5. Print the square of numbers from 1 to 10 using a for loop.
+// for (var i = 1; i <= 10; i++){
+//     console.log(i*i);
+// }
+
+
+// 6. Write a program to reverse a given string using a for loop.
+// var reverse_name = "ihba";
+// var original_name = "";
+// for (var i = reverse_name.length - 1; i >= 0; i--){
+//     original_name += reverse_name[i]
+// }
+// console.log(original_name);
