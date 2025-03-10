136b234facc8fadcb8940f5a940c7ba1910c51a5
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-03-10 16:44:42 +0530

    learned about arrays in java script in lectures javascript folder 11

---
+++ Lectures/Javascript/11/array.js
@@ -0,0 +1,53 @@
+// arrays
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+
+// arrays
+// const arr1 = [];
+// arr1[0] = 1;
+// arr1[1] = 2;
+// arr1[2] = 3;
+// arr1[3] = 4;
+// arr1[4] = 5;
+// console.log(arr1)
+
+
+// assign using new keyword
+// const nums = new Array("abc", 1, 2, 3, "xyz");
+// console.log(nums);
+// console.log(nums.length)
+// console.log(nums.replace('xyz', 'zyx'));
+
+
+// creating objects 
+const student = {name : "Abhi", last_name : "Thul", age : 23};
+// console.log(student);
+// console.log(student.name);
+
+
+
+// looping array elements
+var arr = ["abc", 1, 2, 3, "xyz"];
+// console.log(arr);
+// for (var i = 0; i <= arr.length - 1; i++){
+//     console.log(arr[i]);
+// }
+
+
+// push method
+// arr.push(4);
+// console.log(arr);
+
+
+// pop method
+// arr.pop();
+// console.log(arr);
+
+
+// type of objects
+let type = typeof arr;
+console.log(type);
+
+console.log(typeof student);
+
---
+++ Lectures/Javascript/11/stringMethods.js
@@ -0,0 +1,31 @@
+// split method
+// var sentence = "Hello world";
+// console.log(sentence.length);
+// console.log(sentence.split(" ").length);
+// console.log(sentence.split("o"));
+
+
+// toString method
+// var num = 10;
+// console.log(num.toString());
+
+// toExponential method
+// var a = 2.21314;
+// console.log(a.toExponential())
+// console.log(a.toExponential(2))
+// console.log(a.toExponential(4))
+
+
+// toFix method
+// var b = 3.14123;
+// console.log(b.toFixed());
+// console.log(b.toFixed(3));
+
+
+// parseInt method
+var num = "100";
+console.log(parseInt(num));
+
+// parseFloat method
+var val = "3.14123";
+console.log(parseFloat(val));
