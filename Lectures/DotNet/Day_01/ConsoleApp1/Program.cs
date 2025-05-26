71603dcd37d10db9a7c957126fe2fffeb6dbdf75
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-05-26 15:05:20 +0530

    started C sharp lectures in dotnet folder

---
+++ Lectures/DotNet/Day_01/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World !");
+
+            //string f_name = "Abhi";
+            //string l_name = "Thul";
+            //string full_name = f_name + " " + l_name;
+            //Console.WriteLine("My name is " + f_name + " " + l_name);
+            //Console.WriteLine("My name is " + full_name);
+
+
+            //int num1 = 10;
+            //int num2 = 20;
+            //int num3 = 30;
+            //Console.WriteLine(num1 + num2 + num3);
+
+
+            //int x, y, z;
+            //x = y = z = 15;
+            //Console.WriteLine(x + y + z);
+
+
+            //int x = 20;
+            //Console.WriteLine(x);
+
+            //int x, y, z;
+            //x = 90;
+            //y = 80;
+            //z = 93;
+            //Console.WriteLine("Marks for first object");
+            //Console.WriteLine(x);                
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+            //int a, b, c;
+            //a = 95;
+            //b = 70;
+            //c = 90;
+            //Console.WriteLine("Marks for first object");
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+
+
+            //string name = "John Doe";
+            //int age = 25;
+            //double height = 1.75D;
+            //bool is_student = true;
+            //Console.WriteLine("Name: " + name);
+            //Console.WriteLine("Age: " + age);
+            //Console.WriteLine("Height: " + height);
+            //Console.WriteLine("Is Student: " + is_student);
+
+
+
+
+        }
+    }
+}
