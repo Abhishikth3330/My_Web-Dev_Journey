78607e2d3565edb91101c246d23938ef4cea65af
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-05-30 15:19:46 +0530

    implemented AND or not gates in mysql

--- Lectures/SQL/Tasks/Interview_appointment.sql
+++ Lectures/SQL/Tasks/Interview_appointment.sql
@@ -1,11 +1,10 @@
 show databases;
 
-
-
 use tasks;
 
-
 show tables;
+
+-- 29/05/2025
 
 CREATE TABLE InterviewAppointment (
     id INT,
@@ -31,3 +30,68 @@ VALUES
 
 
 select * from InterviewAppointment;
+
+
+
+
+
+
+
+
+-- 30/05/2025
+
+-- employee_operator
+select * from employee_operator;
+
+-- where clause to check for matching strings from a column
+select * from employee_operator where city='Nagpur';
+
+
+-- where clause to check if the salary is greater than, less than, greater than or equal to, less than or equal to
+select * from employee_operator where salary > 10000;
+
+select * from employee_operator where salary < 10000;
+
+select * from employee_operator where salary >= 13000.45;
+
+select * from employee_operator where salary <= 13000.45;
+
+
+-- not equal to != OR <> Symbol
+select * from employee_operator where salary != 13000.45;
+
+select * from employee_operator where salary <> 13000.45;
+
+
+-- to check if the value lies in BETWEEN some range
+select * from employee_operator where salary BETWEEN 10000 AND 13000;
+
+select * from employee_operator where salary BETWEEN 10000 AND 13100;
+
+
+-- to check string which are LIKE (starts with first letter N...)
+select * from employee_operator where city LIKE 'N%';
+
+-- cities that are like R... 
+select * from employee_operator where city LIKE 'R%';
+
+
+-- checking column having this list, that are IN our column
+select * from employee_operator where city IN ('Nagpur', 'Raipur');
+
+
+
+-- AND
+select * from employee_operator where name='ABC' AND city='Nagpur';
+
+-- OR
+select * from employee_operator where name='JKL' OR city='Nagpur';
+
+-- NOT
+select * from employee_operator where NOT name='JKL';
+
+
+
+
+
+
