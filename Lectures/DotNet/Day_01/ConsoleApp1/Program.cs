f3325a7b99289119eba848368e43712db08dd560
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-05-31 20:56:25 +0530

    solved a question using continue statement

--- Lectures/DotNet/Day_01/ConsoleApp1/Program.cs
+++ Lectures/DotNet/Day_01/ConsoleApp1/Program.cs
@@ -728,8 +728,8 @@ namespace ConsoleApp1
             //        break;
             //    }
             //}
-            
-            
+
+
             //int i = 0;
             //while (i < 10)
             //{
@@ -740,6 +740,21 @@ namespace ConsoleApp1
             //        continue;
             //    }
             //}
+
+
+
+            // pattern - 1
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 3; j++)
+                {
+                    if (j == 2)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine($"i = {i} j = {j}");
+                }
+            }
 
 
 
