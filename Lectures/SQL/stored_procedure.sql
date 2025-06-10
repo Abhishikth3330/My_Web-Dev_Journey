2159de43b57a81ff59272befcce72d5369973a34
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-06-10 16:14:13 +0530

    implemented stored procedure on specific columns according to if else condition

--- Lectures/SQL/stored_procedure.sql
+++ Lectures/SQL/stored_procedure.sql
@@ -41,3 +41,72 @@ call student_info(4, 'JKL', 'Maths', 21);
 call student_info(5, 'MNO', 'Chemistry', 20);
 
 select * from student;
+
+
+
+-- 10/06/2025
+
+create table studentnew(rollno int, name varchar(25), marks int, status varchar(30));
+
+insert into studentnew(rollno, name, marks, status)
+values
+(1, 'ABC', 80, 'Pass'),
+(2, 'Ram', 40, 'Fail'),
+(3, 'Sita', 60, 'Pass'),
+(4, 'Gita', 50, 'Fali');
+
+select * from studentnew;
+
+
+delimiter $$
+create procedure updatestatus1(in marks1 int)
+begin
+declare mymark int;
+set mymark=marks1;
+if mymark>45 then
+update studentnew set status='Pass' where marks=mymark;
+else
+update studentnew set status='Fail' where marks=mymark;
+end if;
+END $$
+delimiter ;
+
+
+call updatestatus1(50);
+
+select * from studentnew;
+
+
+create table student_data(rollno int, name varchar(25), class varchar(10));
+
+alter table student_data add column std_status varchar(100);
+
+select * from student_data;
+
+insert into student_data (rollno, name, class) values
+(1, 'ABC', '10th'),
+(2, 'PQR', '12th'),
+(3, 'TUV', '12th'),
+(4, 'XYZ', '10th');
+
+
+delimiter $$
+create procedure upadte_stdstatus4(in stat1 varchar(100))
+begin
+declare stat varchar(100);
+set stat=stat1;
+if stat='10th' then
+update student_data set std_status='Junior' where class=stat;
+else
+update student_data set std_status='Senior' where class=stat;
+end if;
+END $$
+delimiter ;
+
+
+call upadte_stdstatus4('10th');
+call upadte_stdstatus4('12th');
+
+
+
+
