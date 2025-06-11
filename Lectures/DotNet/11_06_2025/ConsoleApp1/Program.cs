34c1d000ab12f2c7eefca2f643afe448f2fd8c88
Abhishikth3330 <thul_abhishikth.ai@ghrce.raisoni.net>
2025-06-11 15:31:10 +0530

    implemented access modifiers and constructor in c sharp

---
+++ Lectures/DotNet/11_06_2025/ConsoleApp1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //internal class Program
+    //{
+    //}
+
+    //class Car // create class
+    //{
+    //    //public string model; // create field
+
+    //    //public Car(string model_name) // constructor
+    //    //{
+    //    //    //model = "KN";
+    //    //    model = model_name;
+    //    //}
+
+
+
+    //    public string model; // create field
+    //    public string color;
+    //    public int year;
+
+    //    public Car(string model_name, string model_color, int modelyear) // constructor
+    //    {
+    //        //model = "KN";
+    //        model = model_name;
+    //        color = model_color;
+    //        year = modelyear;
+    //    }
+
+    //    static void Main(string[] args)
+    //    {
+    //        //Car car = new Car();
+    //        Car car = new Car("KN", "Red", 1996);
+    //        Console.WriteLine(car.model + car.color + car.year);
+    //    }
+    //}
+
+
+
+
+    // access modifier - private, protected, public
+    //class Car
+    //{
+    //    private string model = "KN";
+
+    //    static void Main(string[] args)
+    //    {
+    //        Car car = new Car();
+    //        Console.Write(car.model);
+    //    }
+    //}
+
+
+
+    // example - 1
+
+    //class Car
+    //{
+    //    public string car_brand;
+    //    public int car_price;
+    //    public Car(string brand, int price)
+    //    {
+    //        car_brand = brand;
+    //        car_price = price;
+    //    }
+    //    static void Main(string[] args)
+    //    {
+    //        Car car = new Car("Buatti", 50000);
+    //        Console.WriteLine($"Brand: {car.car_brand}");
+    //        Console.WriteLine($"Price: {car.car_price}");
+    //    }
+    //}
+
+
+
+    // get-set method
+    class Person
+    {
+        //private string name;
+        public string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myobj = new Person();
+            myobj.Name = "Abhishikth";
+            Console.WriteLine(myobj.Name);
+        }
+    }
+
+
+}
